55ce7a2e5a6cabd3a0f15cc3c1b479bd302e0428
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-05-11 15:50:59 -0500

    updates about.jsx added images

--- React-Portfolio/src/components/About/About.jsx
+++ React-Portfolio/src/components/About/About.jsx
@@ -9,6 +9,24 @@ export const About = () => {
         <ul>
             <li>
                 <img src={getImageUrl("about/gymIcon.png")} alt="gym icon" />
+                <div>
+                    <h3>Gym Life</h3>
+                <p>blah</p>
+                </div>
+            </li>
+            <li>
+                <img src={getImageUrl("about/serverIcon.png")} alt="serverIcon" />
+                <div>
+                    <h3>Full-Stack</h3>
+                <p>Blagh</p>
+                </div>
+            </li>
+            <li>
+                <img src={getImageUrl("about/codeIcon.png")} alt="codeIcon" />
+                <div>
+                    <h3>Future Projects</h3>
+                <p>Blah</p>
+                </div>
             </li>
         </ul>
         </section>
