0a22f94dcd2ba3db661df7057887b163a52ab379
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-05-11 18:51:03 -0500

    some changes

--- React-Portfolio/src/App.jsx
+++ React-Portfolio/src/App.jsx
@@ -2,7 +2,7 @@ import styles from './App.module.css';
 import { Hero } from './components/Hero/Hero';
 import { Navbar } from './components/navbar/Navbar';
 import { About } from './components/About/About';
-import { Projects } from "./components/Projects/Projects";
+import { Projects } from "./components/Applications/Projects";
 
 function App() {
 
