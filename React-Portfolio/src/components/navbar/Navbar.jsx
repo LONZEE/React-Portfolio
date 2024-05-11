9948a55b1820a594f9e9e3f1a1746c41a1fb2189
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-05-10 22:27:37 -0500

    fixed the Navbar.jsx

--- React-Portfolio/src/components/navbar/Navbar.jsx
+++ React-Portfolio/src/components/navbar/Navbar.jsx
@@ -1,14 +1,14 @@
 import React,{ useState } from 'react';
 
 import styles from './Navbar.module.css';
-import { getImagesUrl } from '../../utils';
+import { getImageUrl } from '../../utils';
 
 
 export const Navbar = () => {
     return ( <nav className={styles.navbar}>
         <a className={styles.title} href="/">Portfolio</a>
         <div className={styles.menu}>
-            <img className= {styles.menuBtn} src={getImagesUrl("nav/menuIcon.png")} alt="menu-button" />
+            <img className= {styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt="menu-button" />
             <ul className={styles.menuItems}>
                 <li><a href="#About">About</a></li>
                 <li><a href="#Experience">Experience</a></li>
