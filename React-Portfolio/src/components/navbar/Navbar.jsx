79e146a29a03502fd36b1327b2b1ca83ee265945
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-05-11 19:06:31 -0500

    changes

--- React-Portfolio/src/components/navbar/Navbar.jsx
+++ React-Portfolio/src/components/navbar/Navbar.jsx
@@ -22,8 +22,7 @@ export const Navbar = () => {
                  />
             <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
             onClick ={() => setMenuOpen(false)} >
-                <li><a href="#About">About</a></li>
-                <li><a href="#Experience">Experience</a></li>
+                <li><a href="#about">About</a></li>
                 <li><a href="#Projects">Projects</a></li>
                 <li><a href="#Contact">Contact</a></li>
             </ul>
